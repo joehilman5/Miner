dece8a7f41318f94db2cccbd8ac9b0c9958a6da6
Joe Hilman <joehilman5@gmail.com>
2024-01-26 12:28:53 -0600

    Fixed deployment script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,33 +1,28 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
-// will compile your contracts, add the Hardhat Runtime Environment's members to the
-// global scope, and execute the script.
-const hre = require("hardhat");
+const hre = require('hardhat');
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+	const [owner, second] = await ethers.getSigners();
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
+	const MineralCoin = await ethers.getContractFactory('MineralCoin');
+	const mineralCoin = await MineralCoin.deploy();
+	await mineralCoin.deployed();
+	console.log(`Deployed Mineral Coin Contract at: ${mineralCoin.address}`);
 
-  await lock.waitForDeployment();
+	const GoldCoin = await ethers.getContractFactory('GoldCoin');
+	const goldCoin = await GoldCoin.deploy();
+	await goldCoin.deployed();
+	console.log(`Deployed Gold Coin Contract at: ${goldCoin.address}`);
 
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+	const Miner = await ethers.getContractFactory('Miner');
+	const miner = await Miner.deploy(mineralCoin.address, goldCoin.address);
+	await miner.deployed();
+	console.log(`Deployed Miner Contract at: ${miner.address}`);
+
+
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+	console.error(error);
+	process.exitCode = 1;
 });
